21bdbd5c8692064b36801212a01784c918cc395c
Jackie Wu <wkcn@live.cn>
2017-05-27 16:14:10 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# DinostyPlayer 
+
+## 自动玩Chrome小恐龙游戏
+
+![](./screenshot/1.png)
+
+## 使用
+
+编程语言:python2
+
+第三方库:PyUserInput, PyQt4, numpy, pyopencv2
+
+打开小恐龙游戏，画面放缩比例为100%.
+
+将游戏切换到“重新开始“界面， 即有重新开始按钮的界面。
+![](./button.png)
+
+python2 test.py
+
+## TODO
+
+1. 加入人工神经网络, 让小恐龙自动调参
--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+#coding=utf-8
 from pykeyboard import PyKeyboard
 from PyQt4.QtGui import QImage, QPixmap, QApplication, QColor
 import numpy as np
@@ -26,17 +27,53 @@ while 1:
 '''
 
 app = QApplication(sys.argv)
-im = QPixmap.grabWindow(QApplication.desktop().winId()).toImage()
+game_im = QPixmap.grabWindow(QApplication.desktop().winId()).toImage()
+game_im = QImageToCvMat(game_im)
+btn_im = cv2.imread("button.png")
 
-im = QImageToCvMat(im)
-print im.shape
-cv2.imshow("ha", im)
-cv2.waitKey(0)
-'''
-for x in range(0,20):
-    for y in range(0,20):
-        c = im.pixel(x,y)
-        print c
-        colors = QColor(c).getRgbF()
-        print "(%s,%s) = %s" % (x, y, colors)
-'''
+game_gim = cv2.cvtColor(game_im, cv2.COLOR_BGRA2GRAY)
+btn_gim = cv2.cvtColor(btn_im, cv2.COLOR_BGRA2GRAY)
+
+lower = 80
+upper = 100
+res = (game_im[:,:,0] >= lower) & (game_im[:,:,1] >= lower) & (game_im[:,:,2] >= lower)
+res = res & (game_im[:,:,0] <= upper) & (game_im[:,:,1] <= upper) & (game_im[:,:,2] <= upper)
+game_im[~res] = 255
+
+#kernel = cv2.getStructuringElement(cv2.MORPH_RECT,(5, 5))
+#game_im = cv2.morphologyEx(game_im, cv2.MORPH_OPEN, kernel)
+#game_im = cv2.morphologyEx(game_im, cv2.MORPH_CLOSE, kernel)
+
+res = cv2.matchTemplate(game_gim, btn_gim, cv2.TM_SQDIFF_NORMED)
+min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)  
+print min_val, max_val, min_loc, max_loc
+
+if min_val > 0.10:
+    print ("Not Found Game")
+    sys.exit(0)
+#cv2.imshow("ha2", res)
+#cv2.waitKey(0)
+
+LOC = min_loc
+BUTTON_X = 282
+BUTTON_Y = 75
+SCREEN_W = 600
+SCREEN_H = 150
+SCREEN_X = LOC[0] - BUTTON_X
+SCREEN_Y = LOC[1] - BUTTON_Y
+
+while 1:
+    game_im = QPixmap.grabWindow(QApplication.desktop().winId(), x = SCREEN_X, y = SCREEN_Y, width = SCREEN_W, height = SCREEN_H).toImage()
+    game_im = QImageToCvMat(game_im)
+    CHECK_X = 110
+    CHECK_Y = 113
+    CHECK_W = 5
+    CHECK_H = 5
+    if (game_im[CHECK_Y:CHECK_Y+CHECK_H, CHECK_X:CHECK_X+CHECK_W, 0] != 255).any():
+        kb.tap_key(" ")
+    game_im[CHECK_Y:CHECK_Y+CHECK_H, CHECK_X:CHECK_X+CHECK_W, 0] = 0
+    game_im[CHECK_Y:CHECK_Y+CHECK_H, CHECK_X:CHECK_X+CHECK_W, 1] = 0
+    game_im[CHECK_Y:CHECK_Y+CHECK_H, CHECK_X:CHECK_X+CHECK_W, 2] = 255
+    cv2.imshow("game", game_im)
+    cv2.waitKey(25)
+    #time.sleep(1)
